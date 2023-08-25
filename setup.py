267c372cfd5abeaed419c1173a3bdafa08981a15
niuliangtao <niuliangtao@qq.com>
2023-08-25 14:40:15 +0800

    add

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-import time
-
-from setuptools import find_packages, setup
+from funpypi import setup
 
 install_requires = []
 
-setup(name='farfarfun',
-      version=time.strftime("%Y%m%d%H%M", time.localtime()),
-      description='farfarfun',
-      author='bingtao',
-      author_email='1007530194@qq.com',
-      url='https://github.com/1007530194',
-
-      packages=find_packages(),
-      install_requires=install_requires
-      )
+setup(name="farfarfun", install_requires=install_requires)
